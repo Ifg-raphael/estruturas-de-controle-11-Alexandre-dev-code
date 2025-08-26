a2d3bf381799e03a39f87c9a8f991d091b5f1ce0
Alexandre <alexandre.bessa@academico.ifg.edu.br>
2025-08-26 15:51:29 -0300

    Resolucao

--- classifica_triangulo.py
+++ classifica_triangulo.py
@@ -1,1 +1,22 @@
 # Sua solução aqui
+a = int(input())
+b = int(input())
+c = int(input())
+
+if (a >= b + c) or (b >= a + c) or (c >= b + a):
+    print("Não forma triângulo")
+
+elif (a == b) and (a == c):
+    print("equilátero")
+
+elif (a == b) and (b != c):
+    print("isósceles")
+
+elif (a == c) and (c != b):
+    print("isósceles")
+
+elif (b == c) and (c != a):
+    print("isósceles")
+
+else:
+    print("escaleno")
